c720a357fd86d1630cf26c75216405d37cb04b59
Laiba Nasir <laiba.nasir@eonhealth.com>
2024-10-26 21:44:45 +0500

    initial_project_setup

--- server/server.js
+++ server/server.js
@@ -4,17 +4,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
